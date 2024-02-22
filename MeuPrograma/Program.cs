7b1b94f6d1717c232d94c751c5a3ac07677af592
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-02-22 19:41:54 +0000

    Added comments to the program.

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Contains the program.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Writes "Hello, World!" e "Bye World!" in the command line.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
