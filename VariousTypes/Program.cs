fb8d6b96d42892915689470a92d42a0d3925f288
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-02-22 22:50:16 +0000

    Added char type variables.

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -6,15 +6,27 @@ namespace VariousTypes
     {
         static void Main(string[] args)
         {
+            //Int values
             int i = 12;
             ulong ul = 1UL;
             uint hex = 0x10U;
             int bin = 0b1111100;
 
+            //Writing integer values to console
             Console.WriteLine($"{i} is an int.");
             Console.WriteLine($"{ul} is an ulong.");
             Console.WriteLine($"{hex} is the value of an hexadecimal uint");
             Console.WriteLine($"{bin} is the value of a binary int");
+
+            //Char values
+            char f = '\u2640';
+            char c = '\u2020';
+            char t = '\u2122';
+
+            //Writing character and value
+            Console.WriteLine($"'\\u2640' is the value of {f}.");
+            Console.WriteLine($"'\\u2020' is the value of {c}.");
+            Console.WriteLine($"'\\u2122' is the value of {t}.");
         }
     }
 }
