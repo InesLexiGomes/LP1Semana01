5097532542e58a9fd398f202635816e73d51433f
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-02-22 20:18:42 +0000

    new program with int variables and appropriate literals.

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 12;
+            ulong ul = 1UL;
+            uint hex = 0x10U;
+            int bin = 0b1111100;
+
+            Console.WriteLine($"{i} is an int.");
+            Console.WriteLine($"{ul} is an ulong.");
+            Console.WriteLine($"{hex} is the value of an hexadecimal uint");
+            Console.WriteLine($"{bin} is the value of a binary int");
+        }
+    }
+}
