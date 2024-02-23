3242009b254f46d98c0cc54abfc81e5e9586ecb3
Inês Gomes <a22305790@alunos.ulht.pt>
2024-02-23 13:41:24 +0000

    Added program that aligns the 4 real numbers from the user input.

---
+++ CentraPelaVirgula/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace CentraPelaVirgula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // there will be dots.
+            CultureInfo.DefaultThreadCurrentCulture =
+            CultureInfo.InvariantCulture;
+
+            // asking the user for inputs.
+            Console.WriteLine("Give me 4 real numbers.\n");
+
+            // first input
+            string input1 = Console.ReadLine();
+            float num1 = float.Parse(input1);
+
+            // second input
+            string input2 = Console.ReadLine();
+            float num2 = float.Parse(input2);
+
+    	    // third input
+            string input3 = Console.ReadLine();
+            float num3 = float.Parse(input3);
+
+            // fourth input
+            string input4 = Console.ReadLine();
+            float num4 = float.Parse(input4);
+
+            // writing values
+            Console.WriteLine($"{num1,6:f2}");
+            Console.WriteLine($"{num2,6:f2}");
+            Console.WriteLine($"{num3,6:f2}");
+            Console.WriteLine($"{num4,6:f2}");
+
+        }
+    }
+}
