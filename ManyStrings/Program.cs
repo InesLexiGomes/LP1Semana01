2450c67d96660a23fce381bdb53a052686886c2d
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-02-23 00:23:00 +0000

    New ManyStrings program.

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text; 
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            
+            char w = '\u2640';
+            char s = '\u2704';
+
+            string l = $"me when {w} and I go {s}";
+            string c = "String " + "concatenada :3 " + l;
+            string d = $"Me when {c}";
+
+            Console.WriteLine(l);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+}
