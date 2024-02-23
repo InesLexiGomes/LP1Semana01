499c85d644cb61f6c1acb8abd1de9746488333dd
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-02-23 00:42:20 +0000

    Added formatting to given variables.

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -9,16 +9,28 @@ namespace ManyStrings
         {
             Console.OutputEncoding = Encoding.UTF8;
 
+            // numeric variables
+            double xx = 1.23456;
+            int ii = 19;
+
+            // char variables
             char w = '\u2640';
             char s = '\u2704';
 
+            // string variables
             string l = $"me when {w} and I go {s}";
             string c = "String " + "concatenada :3 " + l;
             string d = $"Me when {c}";
 
+            // console writing
             Console.WriteLine(l);
             Console.WriteLine(c);
             Console.WriteLine(d);
+
+            Console.WriteLine($"{xx:f2}");
+            Console.WriteLine($"{xx:p1}");
+            Console.WriteLine($"{ii:x}");
+            Console.WriteLine($"{ii:c}");
 
             // ex 5 because I thought the other ones would be for 4 :3
             string yippee = "Yippee";
