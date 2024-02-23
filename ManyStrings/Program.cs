a8775055d672cf9394538ad42911b343eccebec2
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-02-23 00:30:15 +0000

    New strings that are merged in mysterious ways.

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -8,7 +8,7 @@ namespace ManyStrings
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            
+
             char w = '\u2640';
             char s = '\u2704';
 
@@ -19,6 +19,16 @@ namespace ManyStrings
             Console.WriteLine(l);
             Console.WriteLine(c);
             Console.WriteLine(d);
+
+            // ex 5 because I thought the other ones would be for 4 :3
+            string yippee = "Yippee";
+            string yippeeSqr = yippee + " " + yippee;
+            string yippeeCool = $"{yippee} {yippee} but cool.";
+
+            Console.WriteLine(yippee);
+            Console.WriteLine(yippeeSqr);
+            Console.WriteLine(yippeeCool);
+
         }
     }
 }
