917e5bea00e8ef7dffb3ed91155103f0fcba45c0
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-02-23 01:07:11 +0000

    Added sum file that requires user input.

---
+++ AskForInput/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AskForInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            Console.WriteLine("Give me an int.");
+            string input1 = Console.ReadLine();
+            int num1 = Convert.ToInt32(input1);
+
+            // second input
+            Console.WriteLine("Give me a float.");
+            string input2 = Console.ReadLine();
+            float num2 = Convert.ToSingle(input2);
+
+            // result
+            Console.WriteLine($"The sum is {num1 + num2}.");
+        }
+    }
+}
